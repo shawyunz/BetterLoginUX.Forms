a5abac4c1f15a7b11aa27e3847c6b6fbdd2bbbb9
shawyunz <shawyunz@gmail.com>
2022-08-12 11:47:53 +1200

    :lipstick: Add images.

---
+++ BetterLoginUX.Forms/BetterLoginUX.Forms/ImageResourceExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BetterLoginUX.Forms
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+            => Source == null ? null : (object)ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+    }
+}
